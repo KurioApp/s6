d92d8be287e2b24c53083550cf43e061a527b546
Rifad Ainun Nazieb <ainun@nazieb.com>
2019-01-21 19:13:54 +0700

    feat: add capability to warm up Thumbor cache

--- agent/main.go
+++ agent/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"io"
@@ -9,6 +12,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/KurioApp/s6"
@@ -27,6 +31,7 @@ var (
 	}
 
 	errForbidden = errors.New("got 503 status")
+	maxHTTPTries = 5
 )
 
 func main() {
@@ -68,39 +73,63 @@ func runServer(cmd *cobra.Command, args []string) {
 			return c.NoContent(http.StatusUnprocessableEntity)
 		}
 
-		go func() {
-			maxTries := 5
-			log := logrus.WithField("file", fileObj)
+		fileType := bucketToType(fileObj.Bucket)
 
-			for i := 0; i < maxTries; i++ {
-				log = log.WithField("tries", i+1)
-				log.Info("Start downloading file")
+		switch fileType {
+		case "video":
+			go processVideo(fileObj)
 
-				err := download(fileObj)
-				if err == errForbidden {
-					log.Info("Got forbidden")
-					time.Sleep(1 * time.Second)
-					continue
+		case "image":
+			go processImage(fileObj)
 
-				}
-
-				if err != nil {
-					log.Error(err)
-				} else {
-					log.Info("Done")
-				}
-
-				return
-			}
-
-			log.Error("Max tries exceeded")
-		}()
+		default:
+			return c.String(http.StatusInternalServerError, "Unknown bucket")
+		}
 
 		return c.NoContent(http.StatusOK)
 	})
 
 	httpAddress := viper.GetString("http_address")
 	e.Start(httpAddress)
+}
+
+func processImage(fileObj s6.S3File) {
+	log := logrus.WithField("file", fileObj)
+
+	paths := viper.GetStringSlice("thumbor.paths")
+	for _, path := range paths {
+		err := hitThumborCache(fileObj, path)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+}
+
+func processVideo(fileObj s6.S3File) {
+	log := logrus.WithField("file", fileObj)
+
+	for i := 0; i < maxHTTPTries; i++ {
+		log = log.WithField("tries", i+1)
+		log.Info("Start downloading file")
+
+		err := download(fileObj)
+		if err == errForbidden {
+			log.Info("Got forbidden")
+			time.Sleep(1 * time.Second)
+			continue
+
+		}
+
+		if err != nil {
+			log.Error(err)
+		} else {
+			log.Info("Done")
+		}
+
+		return
+	}
+
+	log.Error("Max tries exceeded")
 }
 
 func download(fileObj s6.S3File) error {
@@ -147,4 +176,71 @@ func download(fileObj s6.S3File) error {
 	}
 
 	return nil
+}
+
+func hitThumborCache(fileObj s6.S3File, thumborOpt string) error {
+	log := logrus.WithField("file", fileObj)
+
+	thumborURL := viper.GetString("thumbor.url")
+	if thumborURL == "" {
+		return errors.New("thumbor URL not set")
+	}
+
+	originalURL := fmt.Sprintf("https://%s/%s", fileObj.Bucket, fileObj.Key)
+	thumborPath := thumborOpt + "/" + originalURL
+	log = log.WithField("thumbor path", thumborPath)
+
+	thumborKey := viper.GetString("thumbor.key")
+
+	key := "unsafe"
+	if thumborKey != "" {
+		hash := hmac.New(sha1.New, []byte(thumborKey))
+		hash.Write([]byte(thumborPath))
+		message := hash.Sum(nil)
+		key = base64.URLEncoding.EncodeToString(message)
+	}
+
+	imgURL := fmt.Sprintf("%s/%s/%s", thumborURL, key, thumborPath)
+
+	for i := 0; i < maxHTTPTries; i++ {
+		log = log.WithField("tries", i+1)
+
+		resp, err := http.Get(imgURL)
+		if err != nil {
+			return err
+		}
+
+		if resp == nil {
+			return errors.New("got nil response")
+		}
+
+		cacheHeader := strings.ToLower(resp.Header.Get("X-Cache"))
+		if !strings.HasPrefix(cacheHeader, "hit") {
+			log.WithField("headers", resp.Header).Info("Cache not hit")
+			continue
+		} else {
+			log.Info("Cache hit")
+			return nil
+		}
+	}
+
+	return errors.New("max tries exceeded")
+}
+
+func bucketToType(bucket string) string {
+	imgBuckets := viper.GetStringSlice("bucket.image")
+	for _, b := range imgBuckets {
+		if bucket == b {
+			return "image"
+		}
+	}
+
+	videoBuckets := viper.GetStringSlice("bucket.video")
+	for _, b := range videoBuckets {
+		if bucket == b {
+			return "video"
+		}
+	}
+
+	return ""
 }
