69500d2df4af62e8926c4cb6b53f85388acfd273
Rifad Ainun Nazieb <ainun@nazieb.com>
2019-01-21 15:45:37 +0700

    fix: do retry on forbidden status

--- agent/main.go
+++ agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"math/rand"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"time"
 
 	"github.com/KurioApp/s6"
 	"github.com/labstack/echo"
@@ -23,6 +25,8 @@ var (
 		Use: "s6-agent",
 		Run: runServer,
 	}
+
+	errForbidden = errors.New("got 503 status")
 )
 
 func main() {
@@ -64,7 +68,35 @@ func runServer(cmd *cobra.Command, args []string) {
 			return c.NoContent(http.StatusUnprocessableEntity)
 		}
 
-		go download(fileObj)
+		go func() {
+			maxTries := 5
+
+			log := logrus.WithField("file", fileObj)
+			log.Info("Start downloading file")
+
+			for i := 0; i < maxTries; i++ {
+				log = log.WithField("tries", i+1)
+
+				err := download(fileObj)
+				if err == errForbidden {
+					log.Info("Got forbidden")
+					time.Sleep(1 * time.Second)
+					continue
+
+				}
+
+				if err != nil {
+					log.Error(err)
+				} else {
+					log.Info("Done")
+				}
+
+				break
+			}
+
+			log.Error("Max tries exceeded")
+		}()
+
 		return c.NoContent(http.StatusOK)
 	})
 
@@ -72,16 +104,12 @@ func runServer(cmd *cobra.Command, args []string) {
 	e.Start(httpAddress)
 }
 
-func download(fileObj s6.S3File) {
-	log := logrus.WithField("file", fileObj)
-	log.Info("Start downloading file")
-
+func download(fileObj s6.S3File) error {
 	baseDir := viper.GetString("base_dir")
 	fileDir := filepath.Dir(fileObj.Key)
 
 	if err := os.MkdirAll(filepath.Join(baseDir, fileDir), os.ModePerm); err != nil {
-		log.Errorf("failed creating dir: %v", err)
-		return
+		return fmt.Errorf("failed creating dir: %v", err)
 	}
 
 	tempName := fmt.Sprintf("temp-file-%s", strconv.Itoa(rand.Intn(9999999)))
@@ -89,34 +117,35 @@ func download(fileObj s6.S3File) {
 
 	f, err := os.Create(tempFile)
 	if err != nil {
-		log.Errorf("Failed creating file: %v", err)
-		return
+		return fmt.Errorf("failed creating file: %v", err)
 	}
 
 	resp, err := http.Get(fileObj.URL())
 	if err != nil || resp == nil {
-		log.Errorf("Error downloading file: %v", err)
-		return
+		return fmt.Errorf("error downloading file: %v", err)
 	}
 
 	if resp.Body != nil {
 		defer resp.Body.Close()
 	}
 
+	if resp.StatusCode == http.StatusForbidden {
+		return errForbidden
+	}
+
 	if resp.StatusCode != http.StatusOK {
-		log.Errorf("Got non-OK when downloading file: %v", resp.StatusCode)
-		return
+		return fmt.Errorf("got non-OK when downloading file: %v", resp.StatusCode)
 	}
 
 	_, err = io.Copy(f, resp.Body)
 	if err != nil {
-		log.Errorf("Failed storing file: %v", err)
-		return
+		return fmt.Errorf("failed storing file: %v", err)
 	}
 
 	err = os.Rename(tempFile, filepath.Join(baseDir, fileObj.Key))
 	if err != nil {
-		log.Errorf("Failed renaming file: %v", err)
-		return
+		return fmt.Errorf("failed renaming file: %v", err)
 	}
+
+	return nil
 }
